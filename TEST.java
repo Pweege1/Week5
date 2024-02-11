d2dc5416c63f3e5f5e23ee7d3b84157290879930
Pweege1 <157838244+Pweege1@users.noreply.github.com>
2024-02-10 23:55:25 -0600

    Add files via upload

---
+++ TEST.java
@@ -0,0 +1,15 @@
+package TEST;
+
+public class TEST {
+
+	public static void main(String[] args) {}
+		// TODO Auto-generated method stub
+		
+		public interface Logger {
+		    void log(String message);
+		    void error(String message);
+		
+
+	}
+
+}

a0d6fa710eea88fdf5cfba7f0906856f3195b093
Pweege1 <157838244+Pweege1@users.noreply.github.com>
2024-02-10 23:55:05 -0600

    Add files via upload

---
+++ APP.java
@@ -0,0 +1,16 @@
+package TEST;
+
+public class APP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Logger asteriskLogger = new Logger();
+		Logger spacedLogger = new Logger();
+		
+		asteriskLogger.log("Hello");
+		
+		
+
+	}
+
+}
